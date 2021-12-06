68a72989667745e5b87060cd2d2681327d98157f
Bruno T <bruno@dhcp-10-248-109-93.eduroam.wireless.private.cam.ac.uk>
2021-12-06 23:04:40 +0000

    add validationloader

--- dataloaders.py
+++ dataloaders.py
@@ -9,7 +9,7 @@ class DataLoaderDenoising():
         super(DataLoaderDenoising, self).__init__()
         self.trainloader = torch.utils.data.DataLoader(NoisyDataset(dataset_dir), batch_size=batch_size, shuffle=True, num_workers=4)
         self.trainloader_un = torch.utils.data.DataLoader(NoisyDatasetUnsup(dataset_dir_un), batch_size=batch_size, shuffle=True, num_workers=4) # The batch size for unsupervised data is more than supervised data
-        self.validationloader = []
+        self.validationloader = torch.utils.data.DataLoader(NoisyDataset(dataset_dir_un, mode='test', img_size=(320, 320)), batch_size=batch_size, shuffle=True, num_workers=4)
         
     def get_trainloader(self):
         return self.trainloader
