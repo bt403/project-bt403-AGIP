190088024af373bf42400b981c038466ec3124cc
Bruno T <bruno@dhcp-10-248-116-54.eduroam.wireless.private.cam.ac.uk>
2021-12-07 13:19:59 +0000

    add iapdr dataset

--- dataloaders.py
+++ dataloaders.py
@@ -3,6 +3,7 @@ from datasets.dataset_denoising import NoisyDataset, NoisyDatasetUnsup
 
 dataset_dir = "./BSDS300/images"
 dataset_dir_iapdr = "./iaprtc12/images"
+dataset_dir_iapdr_val = "./iaprtc12/additional_images"
 dataset_dir_un = "./SIDD_Small_sRGB_Only"
 
 class DataLoaderDenoising():
@@ -10,7 +11,7 @@ class DataLoaderDenoising():
         super(DataLoaderDenoising, self).__init__()
         self.trainloader = torch.utils.data.DataLoader(NoisyDataset(dataset_dir, dataset_dir_iapdr), batch_size=batch_size, shuffle=True, num_workers=workers)
         self.trainloader_un = torch.utils.data.DataLoader(NoisyDatasetUnsup(dataset_dir_un), batch_size=batch_size, shuffle=True, num_workers=workers) # The batch size for unsupervised data is more than supervised data
-        self.validationloader = torch.utils.data.DataLoader(NoisyDataset(dataset_dir, dataset_dir_iapdr, mode='test', img_size=(320, 320)), batch_size=batch_size, shuffle=True, num_workers=workers)
+        self.validationloader = torch.utils.data.DataLoader(NoisyDataset(dataset_dir, dataset_dir_iapdr_val, mode='test', img_size=(320, 320)), batch_size=batch_size, shuffle=True, num_workers=workers)
         
     def get_trainloader(self):
         return self.trainloader
