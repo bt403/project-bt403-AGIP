5276223a38e3cffa7692fbbcd0be3049a9d26e54
Bruno T <bruno@dhcp-10-248-116-54.eduroam.wireless.private.cam.ac.uk>
2021-12-07 12:50:01 +0000

    add iapdr dataset

--- dataloaders.py
+++ dataloaders.py
@@ -10,7 +10,7 @@ class DataLoaderDenoising():
         super(DataLoaderDenoising, self).__init__()
         self.trainloader = torch.utils.data.DataLoader(NoisyDataset(dataset_dir, dataset_dir_iapdr), batch_size=batch_size, shuffle=True, num_workers=workers)
         self.trainloader_un = torch.utils.data.DataLoader(NoisyDatasetUnsup(dataset_dir_un), batch_size=batch_size, shuffle=True, num_workers=workers) # The batch size for unsupervised data is more than supervised data
-        self.validationloader = torch.utils.data.DataLoader(NoisyDataset(dataset_dir, mode='test', img_size=(320, 320)), batch_size=batch_size, shuffle=True, num_workers=workers)
+        self.validationloader = torch.utils.data.DataLoader(NoisyDataset(dataset_dir, dataset_dir_iapdr, mode='test', img_size=(320, 320)), batch_size=batch_size, shuffle=True, num_workers=workers)
         
     def get_trainloader(self):
         return self.trainloader
