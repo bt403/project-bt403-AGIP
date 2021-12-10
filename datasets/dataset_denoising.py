8ebeead5d48ccecefc3f6273373c301beb6004e9
Bruno T <bruno@dhcp-10-248-124-128.eduroam.wireless.private.cam.ac.uk>
2021-12-10 02:29:05 +0000

    fix noise

--- datasets/dataset_denoising.py
+++ datasets/dataset_denoising.py
@@ -17,7 +17,7 @@ def addNoise(x, sigma=None):
     if sigma is not None:
       noiseLevel = sigma
     else:
-      noiseLevel = rand.choice([0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 75])
+      noiseLevel = rand.choice([0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75])
     #return x + normal.sample(sample_shape=torch.Size(x.shape)).to(device)
     return x + (torch.randn(x.shape) * noiseLevel)
 
