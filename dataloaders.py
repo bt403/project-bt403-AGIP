68693679d3f3b1d5ad48911b359eb29a8fae788c
Bruno T <bruno@dhcp-10-248-124-128.eduroam.wireless.private.cam.ac.uk>
2021-12-10 00:40:20 +0000

    new dataset test

--- dataloaders.py
+++ dataloaders.py
@@ -6,16 +6,19 @@ dataset_dir_iapdr = "./iaprtc12/images"
 dataset_dir_iapdr_val = "./iaprtc12/additional_images"
 dataset_dir_un = "./SIDD_Small_sRGB_Only"
 dataset_dir_un_coco = "./unlabeled2017"
+
 dataset_dir_sup_1 = "./BBSDS500/train"
 dataset_dir_sup_2 = "./exploration_database"
 dataset_dir_sup_3 = "./imagenet-400"
+dataset_dir_un_1_gt = "./CBSD68/original_png"
+dataset_dir_un_1_noisy = "./CBSD68/noisy50"
 
 class DataLoaderDenoising():
     def __init__(self, batch_size, batch_size_unsup, workers):
         super(DataLoaderDenoising, self).__init__()
         self.trainloader = torch.utils.data.DataLoader(NoisyDataset(dataset_dir_sup_1, dataset_dir_sup_2, dataset_dir_sup_3, 'train', (50,50), 30), batch_size=batch_size, shuffle=True, num_workers=workers)
         self.trainloader_un = torch.utils.data.DataLoader(NoisyDatasetUnsup(dataset_dir_un, dataset_dir_un_coco), batch_size=batch_size_unsup, shuffle=True, num_workers=workers) # The batch size for unsupervised data is more than supervised data
-        #self.validationloader = torch.utils.data.DataLoader(NoisyDataset(dataset_dir, dataset_dir_iapdr_val, None, mode='test', img_size=(320, 320)), batch_size=batch_size, shuffle=True, num_workers=workers)
+        self.validationloader = torch.utils.data.DataLoader(NoisyDatasetUn(dataset_dir_un_1_gt, dataset_dir_un_1_noisy, mode='test', img_size=(320, 320)), batch_size=batch_size, shuffle=True, num_workers=workers)
         
     def get_trainloader(self):
         return self.trainloader
--- datasets/dataset_denoising.py
+++ datasets/dataset_denoising.py
@@ -7,14 +7,17 @@ import random as rand
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 
-def addNoise(x, device): 
+def addNoise(x, sigma=None): 
     """
     We will use this helper function to add noise to some data. 
     x: the data we want to add noise to
     device: the CPU or GPU that the input is located on. 
     """
-    noiseLevel = rand.choice([0.5])
-    #noiseLevel = rand.choice([0, 0.1, 0.3, 0.5])
+    #noiseLevel = rand.choice([0.5])
+    if sigma is not None:
+      noiseLevel = sigma
+    else:
+      noiseLevel = rand.choice([0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 75])
     #return x + normal.sample(sample_shape=torch.Size(x.shape)).to(device)
     return x + (torch.randn(x.shape) * noiseLevel)
 
@@ -23,9 +26,9 @@ class NoisyDataset(torch.utils.data.Dataset):
     super(NoisyDataset, self).__init__()
 
     self.mode = mode #train or test
-    self.in_path_data_1 = in_path_data_1 # ./BSDS300/images
-    self.in_path_data_2 = in_path_data_2 # ./BSDS300/images
-    self.in_path_data_3 = in_path_data_3 # ./BSDS300/images
+    self.in_path_data_1 = in_path_data_1
+    self.in_path_data_2 = in_path_data_2
+    self.in_path_data_3 = in_path_data_3
     self.img_size = img_size # (180, 180)
 
     self.imgs_data_1 = os.listdir(self.in_path_data_1)
@@ -66,11 +69,76 @@ class NoisyDataset(torch.utils.data.Dataset):
         top = 0
     # .crop(left, upper, right, lower)
     cropped_clean = clean_img.crop([left, top, left+self.img_size[0], top+self.img_size[1]])
+    transform = tv.transforms.Compose([
+                                    tv.transforms.RandomHorizontalFlip(p=0.5),
+                                    tv.transforms.ToTensor(),
+                                    tv.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    ground_truth = transform(cropped_clean)
+    noisy = addNoise(ground_truth)
+    return noisy, ground_truth
+
+
+class NoisyDatasetUn(torch.utils.data.Dataset):
+  def __init__(self, in_path_gt, in_path_noisy, mode='train', img_size=(320, 320), sigma=30):
+    super(NoisyDatasetUnsup, self).__init__()
+
+    self.mode = mode #train or test
+    self.in_path_gt = in_path_gt #
+    self.in_path_noisy = in_path_noisy #
+    self.img_size = img_size # (180, 180)
+
+    self.imgs_gt = os.listdir(self.in_path_gt)
+    self.imgs_noisy = os.listdir(self.in_path_noisy)
+    
+    self.imgs_path_gt = list()
+    for i in self.imgs_gt:
+      _, ext = os.path.splitext(i)
+      if ext in [".jpg", ".jpg", ".bmp", ".JPEG", ".jpeg", ".png"]:
+        self.imgs_path_gt.append(os.path.join(self.in_path_gt, i))
+
+    self.imgs_path_noisy = list()
+    for i in self.imgs_noisy:
+      _, ext = os.path.splitext(i)
+      if ext in [".jpg", ".jpg", ".bmp", ".JPEG", ".jpeg", ".png"]:
+        self.imgs_path_noisy.append(os.path.join(self.in_path_noisy, i))
+
+  def __len__(self):
+    return len(self.imgs_path_gt)
+
+  def __getitem__(self, idx):
+    #img_path = os.path.join(self.img_dir, self.imgs[idx])
+    img_path_gt = self.imgs_path_gt[idx]
+    img_path_noisy = self.imgs_path_noisy[idx]
+    clean_img = Image.open(img_path_gt).convert('RGB')
+    noisy_img = Image.open(img_path_noisy).convert('RGB')
+
+    if (clean_img.size[0] > self.img_size[0]):
+        left = np.random.randint(clean_img.size[0] - self.img_size[0])
+    else:
+        left = 0
+    if (clean_img.size[1] > self.img_size[1]):
+        top = np.random.randint(clean_img.size[1] - self.img_size[1])
+    else:
+        top = 0
+    # .crop(left, upper, right, lower)
+    cropped_clean = clean_img.crop([left, top, left+self.img_size[0], top+self.img_size[1]])
+    cropped_noisy = noisy_img.crop([left, top, left+self.img_size[0], top+self.img_size[1]])
     transform = tv.transforms.Compose([tv.transforms.ToTensor(),
                                     tv.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
     ground_truth = transform(cropped_clean)
-    noisy = addNoise(ground_truth, device)
+    noisy = transform(cropped_noisy)
+    #noisy = addNoise(ground_truth)
     return noisy, ground_truth
+
+
+
+
+
+
+
+
+
+
 
 
 class NoisyDatasetUnsup(torch.utils.data.Dataset):
@@ -117,5 +185,5 @@ class NoisyDatasetUnsup(torch.utils.data.Dataset):
     transform = tv.transforms.Compose([tv.transforms.ToTensor(),
                                     tv.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
     ground_truth = transform(cropped_clean)
-    noisy = addNoise(ground_truth, device)
+    noisy = addNoise(ground_truth)
     return noisy, ground_truth
