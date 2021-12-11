8fe0d1b3ceb12d6738ff02e56732bec93697ec28
Bruno T <bruno@dhcp-10-248-123-6.eduroam.wireless.private.cam.ac.uk>
2021-12-11 22:23:20 +0000

    update fdnet model

--- models/FFDNet.py
+++ models/FFDNet.py
@@ -1,157 +1,193 @@
-#https://github.com/cszn/FFDNet/
-import torch
+"""
+Definition of the FFDNet model and its custom layers
+
+Copyright (C) 2018, Matias Tassano <matias.tassano@parisdescartes.fr>
+
+This program is free software: you can use, modify and/or
+redistribute it under the terms of the GNU General Public
+License as published by the Free Software Foundation, either
+version 3 of the License, or (at your option) any later
+version. You should have received a copy of this license along
+this program. If not, see <http://www.gnu.org/licenses/>.
+"""
 import torch.nn as nn
-import numpy as np
+from torch.autograd import Variable
+import torch
+from torch.autograd import Function, Variable
 
-sigma = torch.full((1,1,1,1), 15/255.)
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+def concatenate_input_noise_map(input, noise_sigma):
+	r"""Implements the first layer of FFDNet. This function returns a
+	torch.autograd.Variable composed of the concatenation of the downsampled
+	input image and the noise map. Each image of the batch of size CxHxW gets
+	converted to an array of size 4*CxH/2xW/2. Each of the pixels of the
+	non-overlapped 2x2 patches of the input image are placed in the new array
+	along the first dimension.
 
-from collections import OrderedDict
+	Args:
+		input: batch containing CxHxW images
+		noise_sigma: the value of the pixels of the CxH/2xW/2 noise map
+	"""
+	# noise_sigma is a list of length batch_size
+	N, C, H, W = input.size()
+	dtype = input.type()
+	sca = 2
+	sca2 = sca*sca
+	Cout = sca2*C
+	Hout = H//sca
+	Wout = W//sca
+	idxL = [[0, 0], [0, 1], [1, 0], [1, 1]]
 
-def sequential(*args):
-    """Advanced nn.Sequential.
-    Args:
-        nn.Sequential, nn.Module
-    Returns:
-        nn.Sequential
-    """
-    if len(args) == 1:
-        if isinstance(args[0], OrderedDict):
-            raise NotImplementedError('sequential does not support OrderedDict input.')
-        return args[0]  # No sequential is needed.
-    modules = []
-    for module in args:
-        if isinstance(module, nn.Sequential):
-            for submodule in module.children():
-                modules.append(submodule)
-        elif isinstance(module, nn.Module):
-            modules.append(module)
-    return nn.Sequential(*modules)
+	# Fill the downsampled image with zeros
+	if 'cuda' in dtype:
+		downsampledfeatures = torch.cuda.FloatTensor(N, Cout, Hout, Wout).fill_(0)
+	else:
+		downsampledfeatures = torch.FloatTensor(N, Cout, Hout, Wout).fill_(0)
 
-def conv(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=True, mode='CBR', negative_slope=0.2):
-    L = []
-    for t in mode:
-        if t == 'C':
-            L.append(nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias))
-        elif t == 'T':
-            L.append(nn.ConvTranspose2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias))
-        elif t == 'B':
-            L.append(nn.BatchNorm2d(out_channels, momentum=0.9, eps=1e-04, affine=True))
-        elif t == 'I':
-            L.append(nn.InstanceNorm2d(out_channels, affine=True))
-        elif t == 'R':
-            L.append(nn.ReLU(inplace=True))
-        elif t == 'r':
-            L.append(nn.ReLU(inplace=False))
-        elif t == 'L':
-            L.append(nn.LeakyReLU(negative_slope=negative_slope, inplace=True))
-        elif t == 'l':
-            L.append(nn.LeakyReLU(negative_slope=negative_slope, inplace=False))
-        elif t == '2':
-            L.append(nn.PixelShuffle(upscale_factor=2))
-        elif t == '3':
-            L.append(nn.PixelShuffle(upscale_factor=3))
-        elif t == '4':
-            L.append(nn.PixelShuffle(upscale_factor=4))
-        elif t == 'U':
-            L.append(nn.Upsample(scale_factor=2, mode='nearest'))
-        elif t == 'u':
-            L.append(nn.Upsample(scale_factor=3, mode='nearest'))
-        elif t == 'v':
-            L.append(nn.Upsample(scale_factor=4, mode='nearest'))
-        elif t == 'M':
-            L.append(nn.MaxPool2d(kernel_size=kernel_size, stride=stride, padding=0))
-        elif t == 'A':
-            L.append(nn.AvgPool2d(kernel_size=kernel_size, stride=stride, padding=0))
-        else:
-            raise NotImplementedError('Undefined type: '.format(t))
-    return sequential(*L)
+	# Build the CxH/2xW/2 noise map
+	noise_map = noise_sigma.view(N, 1, 1, 1).repeat(1, C, Hout, Wout)
 
-def pixel_unshuffle(input, upscale_factor):
-    r"""Rearranges elements in a Tensor of shape :math:`(C, rH, rW)` to a
-    tensor of shape :math:`(*, r^2C, H, W)`.
-    Authors:
-        Zhaoyi Yan, https://github.com/Zhaoyi-Yan
-        Kai Zhang, https://github.com/cszn/FFDNet
-    Date:
-        01/Jan/2019
-    """
-    batch_size, channels, in_height, in_width = input.size()
+	# Populate output
+	for idx in range(sca2):
+		downsampledfeatures[:, idx:Cout:sca2, :, :] = \
+			input[:, :, idxL[idx][0]::sca, idxL[idx][1]::sca]
 
-    out_height = in_height // upscale_factor
-    out_width = in_width // upscale_factor
+	# concatenate de-interleaved mosaic with noise map
+	return torch.cat((noise_map, downsampledfeatures), 1)
 
-    input_view = input.contiguous().view(
-        batch_size, channels, out_height, upscale_factor,
-        out_width, upscale_factor)
+class UpSampleFeaturesFunction(Function):
+	r"""Extends PyTorch's modules by implementing a torch.autograd.Function.
+	This class implements the forward and backward methods of the last layer
+	of FFDNet. It basically performs the inverse of
+	concatenate_input_noise_map(): it converts each of the images of a
+	batch of size CxH/2xW/2 to images of size C/4xHxW
+	"""
+	@staticmethod
+	def forward(ctx, input):
+		N, Cin, Hin, Win = input.size()
+		dtype = input.type()
+		sca = 2
+		sca2 = sca*sca
+		Cout = Cin//sca2
+		Hout = Hin*sca
+		Wout = Win*sca
+		idxL = [[0, 0], [0, 1], [1, 0], [1, 1]]
 
-    channels *= upscale_factor ** 2
-    unshuffle_out = input_view.permute(0, 1, 3, 5, 2, 4).contiguous()
-    return unshuffle_out.view(batch_size, channels, out_height, out_width)
+		assert (Cin%sca2 == 0), \
+			'Invalid input dimensions: number of channels should be divisible by 4'
 
-class PixelUnShuffle(nn.Module):
-    r"""Rearranges elements in a Tensor of shape :math:`(C, rH, rW)` to a
-    tensor of shape :math:`(*, r^2C, H, W)`.
-    Authors:
-        Zhaoyi Yan, https://github.com/Zhaoyi-Yan
-        Kai Zhang, https://github.com/cszn/FFDNet
-    Date:
-        01/Jan/2019
-    """
+		result = torch.zeros((N, Cout, Hout, Wout)).type(dtype)
+		for idx in range(sca2):
+			result[:, :, idxL[idx][0]::sca, idxL[idx][1]::sca] = \
+				input[:, idx:Cin:sca2, :, :]
 
-    def __init__(self, upscale_factor):
-        super(PixelUnShuffle, self).__init__()
-        self.upscale_factor = upscale_factor
+		return result
 
-    def forward(self, input):
-        return pixel_unshuffle(input, self.upscale_factor)
+	@staticmethod
+	def backward(ctx, grad_output):
+		N, Cg_out, Hg_out, Wg_out = grad_output.size()
+		dtype = grad_output.data.type()
+		sca = 2
+		sca2 = sca*sca
+		Cg_in = sca2*Cg_out
+		Hg_in = Hg_out//sca
+		Wg_in = Wg_out//sca
+		idxL = [[0, 0], [0, 1], [1, 0], [1, 1]]
 
-    def extra_repr(self):
-        return 'upscale_factor={}'.format(self.upscale_factor)
+		# Build output
+		grad_input = torch.zeros((N, Cg_in, Hg_in, Wg_in)).type(dtype)
+		# Populate output
+		for idx in range(sca2):
+			grad_input[:, idx:Cg_in:sca2, :, :] = \
+				grad_output.data[:, :, idxL[idx][0]::sca, idxL[idx][1]::sca]
 
+		return Variable(grad_input)
+
+# Alias functions
+upsamplefeatures = UpSampleFeaturesFunction.apply
+
+
+class UpSampleFeatures(nn.Module):
+	r"""Implements the last layer of FFDNet
+	"""
+	def __init__(self):
+		super(UpSampleFeatures, self).__init__()
+	def forward(self, x):
+		return upsamplefeatures(x)
+
+class IntermediateDnCNN(nn.Module):
+	r"""Implements the middel part of the FFDNet architecture, which
+	is basically a DnCNN net
+	"""
+	def __init__(self, input_features, middle_features, num_conv_layers):
+		super(IntermediateDnCNN, self).__init__()
+		self.kernel_size = 3
+		self.padding = 1
+		self.input_features = input_features
+		self.num_conv_layers = num_conv_layers
+		self.middle_features = middle_features
+		if self.input_features == 5:
+			self.output_features = 4 #Grayscale image
+		elif self.input_features == 15:
+			self.output_features = 12 #RGB image
+		else:
+			raise Exception('Invalid number of input features')
+
+		layers = []
+		layers.append(nn.Conv2d(in_channels=self.input_features,\
+								out_channels=self.middle_features,\
+								kernel_size=self.kernel_size,\
+								padding=self.padding,\
+								bias=False))
+		layers.append(nn.ReLU(inplace=True))
+		for _ in range(self.num_conv_layers-2):
+			layers.append(nn.Conv2d(in_channels=self.middle_features,\
+									out_channels=self.middle_features,\
+									kernel_size=self.kernel_size,\
+									padding=self.padding,\
+									bias=False))
+			layers.append(nn.BatchNorm2d(self.middle_features))
+			layers.append(nn.ReLU(inplace=True))
+		layers.append(nn.Conv2d(in_channels=self.middle_features,\
+								out_channels=self.output_features,\
+								kernel_size=self.kernel_size,\
+								padding=self.padding,\
+								bias=False))
+		self.itermediate_dncnn = nn.Sequential(*layers)
+	def forward(self, x):
+		out = self.itermediate_dncnn(x)
+		return out
 
 class FFDNet(nn.Module):
-    def __init__(self, in_nc=1, out_nc=1, nc=64, nb=15, act_mode='R'):
-        """
-        # ------------------------------------
-        in_nc: channel number of input
-        out_nc: channel number of output
-        nc: channel number
-        nb: total number of conv layers
-        act_mode: batch norm + activation function; 'BR' means BN+ReLU.
-        # ------------------------------------
-        # ------------------------------------
-        """
-        super(FFDNet, self).__init__()
-        assert 'R' in act_mode or 'L' in act_mode, 'Examples of activation function: R, L, BR, BL, IR, IL'
-        bias = True
-        sf = 2
+	r"""Implements the FFDNet architecture
+	"""
+	def __init__(self, num_input_channels):
+		super(FFDNet, self).__init__()
+		self.num_input_channels = num_input_channels
+		if self.num_input_channels == 1:
+			# Grayscale image
+			self.num_feature_maps = 64
+			self.num_conv_layers = 15
+			self.downsampled_channels = 5
+			self.output_features = 4
+		elif self.num_input_channels == 3:
+			# RGB image
+			self.num_feature_maps = 96
+			self.num_conv_layers = 12
+			self.downsampled_channels = 15
+			self.output_features = 12
+		else:
+			raise Exception('Invalid number of input features')
 
-        self.m_down = PixelUnShuffle(upscale_factor=sf)
+		self.intermediate_dncnn = IntermediateDnCNN(\
+				input_features=self.downsampled_channels,\
+				middle_features=self.num_feature_maps,\
+				num_conv_layers=self.num_conv_layers)
+		self.upsamplefeatures = UpSampleFeatures()
 
-        m_head = conv(in_nc*sf*sf+1, nc, mode='C'+act_mode[-1], bias=bias)
-        m_body = [conv(nc, nc, mode='C'+act_mode, bias=bias) for _ in range(nb-2)]
-        m_tail = conv(nc, out_nc*sf*sf, mode='C', bias=bias)
-
-        self.model = sequential(m_head, *m_body, m_tail)
-
-        self.m_up = nn.PixelShuffle(upscale_factor=sf)
-    
-    def forward(self, x, sigma=sigma, b_size=10):
-        sigma = sigma.to(device)
-        h, w = x.size()[-2:]
-        paddingBottom = int(np.ceil(h/2)*2-h)
-        paddingRight = int(np.ceil(w/2)*2-w)
-        x = torch.nn.ReplicationPad2d((0, paddingRight, 0, paddingBottom))(x)
-
-        x = self.m_down(x)
-        m = torch.ones(b_size, sigma.size()[1], x.size()[-2], x.size()[-1]).type_as(x).mul(sigma)
-        #m = sigma.repeat(1, 1, x.size()[-2], x.size()[-1]).to(device)
-        #print(x.shape)
-        #print(m.shape)
-        x = torch.cat((x, m), 1)
-        x = self.model(x)
-        x = self.m_up(x)
-        
-        x = x[..., :h, :w]
-        return x
+	def forward(self, x, noise_sigma):
+		concat_noise_x = concatenate_input_noise_map(\
+				x.data, noise_sigma.data)
+		concat_noise_x = Variable(concat_noise_x)
+		h_dncnn = self.intermediate_dncnn(concat_noise_x)
+		pred_noise = self.upsamplefeatures(h_dncnn)
+		return pred_noise
--- train_denoising.py
+++ train_denoising.py
@@ -6,8 +6,10 @@ from parsing import get_parser
 from models.FFDNet import FFDNet
 from dataloaders import DataLoaderDenoising
 from tqdm import tqdm
+import math
+import numpy as np
+from torch.autograd import Variable
 import wandb
-
 
 wandb.init(project="my-test-project", entity="btafur")
 
@@ -22,9 +24,34 @@ wandb.config = {
 }
 wandb.init()
 
-denoise_model = FFDNet(3,3,96,15).to(device)
+def weights_init_kaiming(lyr):
+	r"""Initializes weights of the model according to the "He" initialization
+	method described in "Delving deep into rectifiers: Surpassing human-level
+    performance on ImageNet classification" - He, K. et al. (2015), using a
+    normal distribution.
+	This function is to be called by the torch.nn.Module.apply() method,
+	which applies weights_init_kaiming() to every layer of the model.
+	"""
+	classname = lyr.__class__.__name__
+	if classname.find('Conv') != -1:
+		nn.init.kaiming_normal(lyr.weight.data, a=0, mode='fan_in')
+	elif classname.find('Linear') != -1:
+		nn.init.kaiming_normal(lyr.weight.data, a=0, mode='fan_in')
+	elif classname.find('BatchNorm') != -1:
+		lyr.weight.data.normal_(mean=0, std=math.sqrt(2./9./64.)).\
+			clamp_(-0.025, 0.025)
+		nn.init.constant(lyr.bias.data, 0.0)
+
+denoise_model = FFDNet(3).to(device)
+denoise_model.apply(weights_init_kaiming)
 tcr = TCR().to(device)
 optimizer = torch.optim.Adam(denoise_model.parameters(), lr =args.lr)
+
+criterion_mse = nn.MSELoss(size_average=False).to(device)
+criterion_l1Loss = nn.L1Loss().to(device)
+
+device_ids = [0]
+denoise_model_p = nn.DataParallel(denoise_model, device_ids=device_ids).cuda()
 
 if (args.resume > 0):
     checkpoint = torch.load(args.checkpoint)
@@ -33,10 +60,7 @@ if (args.resume > 0):
     epoch = checkpoint['epoch']
     loss = checkpoint['loss']
 
-denoise_model.train()
-
-criterion_mse = nn.MSELoss().to(device)
-criterion_l1Loss = nn.L1Loss().to(device)
+denoise_model_p.train()
 
 dataLoaderDenoising = DataLoaderDenoising(args.batch_size, args.batch_size_un, args.workers)
 trainloader = dataLoaderDenoising.get_trainloader()
@@ -48,28 +72,51 @@ def validate():
     with torch.no_grad():
         for batch in validationloader:
             input, target = batch[0].to(device), batch[1].to(device)
-            prediction = denoise_model(input, b_size=len(input))
-            mse = criterion_mse(prediction, target)
+            img_val = torch.unsqueeze(input, 0)
+            noise = torch.FloatTensor(img_val.size()).normal_(mean=0, std=args.val_noiseL)
+            imgn_val = img_val + noise
+            img_val, imgn_val = Variable(img_val.cuda()), Variable(imgn_val.cuda())
+            sigma_noise = Variable(torch.cuda.FloatTensor([args.val_noiseL]))
+            out_val = torch.clamp(imgn_val-denoise_model_p(imgn_val, sigma_noise), 0., 1.)
+            mse = criterion_mse(out_val, target)
             psnr = 10 * torch.log10(1 / mse)
             avg_psnr += psnr
     wandb.log({"psnr": psnr}) 
     print("===> Avg. PSNR: {:.4f} dB".format(avg_psnr / len(validationloader)))
 
-def train(data_sup, data_un, denoise_model, running_loss, with_tcr, step):
+def train(data_sup, data_un, denoise_model_p, running_loss, with_tcr, step):
     b_size = data_sup[0].shape[0]
     input, target = data_sup[0].to('cuda:0', non_blocking=True), data_sup[1].to('cuda:0', non_blocking=True)  # Here the data is used in supervised fashion
     if (with_tcr):
         b_size_unsup = data_un[0].shape[0]
         input_un, target_un = data_un[0].to('cuda:0', non_blocking=True), data_un[1].to('cuda:0', non_blocking=True)  # Here the labels are not used
     optimizer.zero_grad()
-    outputs = denoise_model(input, b_size=b_size)
-    loss = criterion_mse(outputs,target)
+    outputs = denoise_model_p(input, b_size=b_size)
+
+    #input = data
+    noise = torch.zeros(input.size())
+    stdn = np.random.uniform(args.noiseIntL[0], args.noiseIntL[1], \
+                    size=noise.size()[0])
+    for nx in range(noise.size()[0]):
+        sizen = noise[0, :, :, :].size()
+        noise[nx, :, :, :] = torch.FloatTensor(sizen).\
+                            normal_(mean=0, std=stdn[nx])
+    imgn_train = input + noise
+    # Create input Variables
+    input = Variable(input.cuda())
+    imgn_train = Variable(imgn_train.cuda())
+    noise = Variable(noise.cuda())
+    stdn_var = Variable(torch.cuda.FloatTensor(stdn))
+
+    # Evaluate model and optimize it
+    out_train = denoise_model_p(imgn_train, stdn_var)
+    loss = criterion_mse(out_train, noise) / (imgn_train.size()[0]*2)
     
     if with_tcr:
         bs = input_un.shape[0]
         random = torch.rand((bs, 1))
         transformed_input = tcr(input_un,random.to('cuda:0', non_blocking=True))
-        loss_tcr = criterion_mse(denoise_model(transformed_input, b_size=b_size_unsup), tcr(denoise_model(input_un, b_size=b_size_unsup),random))
+        loss_tcr = criterion_mse(denoise_model_p(transformed_input, b_size=b_size_unsup), tcr(denoise_model_p(input_un, b_size=b_size_unsup),random))
         total_loss= loss + args.weight_tcr*loss_tcr
     else:
         total_loss= loss
@@ -91,10 +138,10 @@ if args.with_tcr > 0:
         total_iter = min(len(trainloader), len(trainloader_un))
         step_log = 0
         for iteration, (data_sup, data_un) in enumerate(tqdm(zip(trainloader, trainloader_un), total=total_iter)):
-            running_loss, loss_tcr = train(data_sup, data_un, denoise_model, running_loss, args.with_tcr, step_log)
+            running_loss, loss_tcr = train(data_sup, data_un, denoise_model_p, running_loss, args.with_tcr, step_log)
             step_log += 1
         if ((epoch+1)%10 == 0):
-            torch.save(denoise_model.state_dict(), "./checkpoint/denoise_checkpoint_with_tcr_" + str(epoch+1)+ ".pt")
+            torch.save(denoise_model_p.state_dict(), "./checkpoint/denoise_checkpoint_with_tcr_" + str(epoch+1)+ ".pt")
         print('Epoch-{0} lr: {1}'.format(epoch+1, optimizer.param_groups[0]['lr']))
         print('[%d] total loss: %.3f' % (epoch + 1, running_loss ))     
         print('tcr loss: %.3f' % (loss_tcr))  
@@ -105,13 +152,13 @@ else:
         total_iter = len(trainloader)
         step_log = 0
         for iteration, data_sup in enumerate(tqdm(trainloader, total=total_iter)):
-            running_loss = train(data_sup, None, denoise_model, running_loss, args.with_tcr, step_log)
+            running_loss = train(data_sup, None, denoise_model_p, running_loss, args.with_tcr, step_log)
             step_log += 1
         if ((epoch+1)%2 == 0):
-            torch.save(denoise_model.state_dict(), "./checkpoint/denoise_checkpoint_" + str(epoch+1)+ ".pt")
+            torch.save(denoise_model_p.state_dict(), "./checkpoint/denoise_checkpoint_" + str(epoch+1)+ ".pt")
             torch.save({
             'epoch': epoch,
-            'model_state_dict': denoise_model.state_dict(),
+            'model_state_dict': denoise_model_p.state_dict(),
             'optimizer_state_dict': optimizer.state_dict(),
             'loss': running_loss,
             }, "./checkpoint/denoise_checkpoint_resume_" + str(epoch+1)+ ".tar")
