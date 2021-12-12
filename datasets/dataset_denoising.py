a7a7abcb353d98cb88ccf030312b71dfefbbcedc
Bruno T <bruno@dhcp-10-248-123-6.eduroam.wireless.private.cam.ac.uk>
2021-12-12 02:38:40 +0000

    update fdnet model

--- datasets/dataset_denoising.py
+++ datasets/dataset_denoising.py
@@ -68,11 +68,12 @@ class NoisyDataset(torch.utils.data.Dataset):
         top = 0
     # .crop(left, upper, right, lower)
     cropped_clean = clean_img.crop([left, top, left+self.img_size[0], top+self.img_size[1]])
-    cropped_clean = np.array(cropped_clean) / 255.
+   
     transform = tv.transforms.Compose([
                                     tv.transforms.RandomHorizontalFlip(p=0.5),
                                     tv.transforms.ToTensor(),])
     ground_truth = transform(cropped_clean)
+    ground_truth /= 255.
     #noisy = addNoise(ground_truth)
     noisy = ground_truth
     return noisy, ground_truth
@@ -125,9 +126,9 @@ class NoisyDatasetUn(torch.utils.data.Dataset):
     # .crop(left, upper, right, lower)
     cropped_clean = clean_img.crop([left, top, left+self.img_size[0], top+self.img_size[1]])
     cropped_noisy = noisy_img.crop([left, top, left+self.img_size[0], top+self.img_size[1]])
-    cropped_clean = np.array(cropped_clean) / 255.
     transform = tv.transforms.Compose([tv.transforms.ToTensor(),])
     ground_truth = transform(cropped_clean)
+    ground_truth /= 255.
     #noisy = transform(cropped_noisy)
     return ground_truth, ground_truth
 
