a7d9f8b0fa922c91671c2241146879444dc88f4a
Bruno T <bruno@Brunos-MacBook-Pro.local>
2021-12-29 00:52:41 +0000

    update unsupervised dataloader

--- dataloaders.py
+++ dataloaders.py
@@ -1,25 +1,26 @@
 import torch
-from datasets.dataset_denoising import NoisyDataset, NoisyDatasetUnsup, NoisyDatasetUn
+from datasets.dataset_denoising import NoisyDataset, NoisyDatasetUn, NoisyDatasetVal
 
 dataset_dir = "./BSDS300/images"
 dataset_dir_iapdr = "./iaprtc12/images"
 dataset_dir_iapdr_val = "./iaprtc12/additional_images"
 dataset_dir_un = "./SIDD_Small_sRGB_Only"
-dataset_dir_un_coco = "./unlabeled2017"
 
+dataset_dir_un_coco = "./unlabeled2017"
 dataset_dir_sup_1 = "./BBSDS500/train"
 dataset_dir_sup_2 = "./exploration_database"
 dataset_dir_sup_3 = "./imagenet-400"
-dataset_dir_un_1_gt = "./CBSD68/original_png/original_png"
-dataset_dir_un_1_noisy = "./CBSD68/noisy50"
+dataset_dir_val_1_gt = "./CBSD68/original_png/original_png"
+dataset_dir_val_1_noisy = "./CBSD68/noisy50"
+
 
 class DataLoaderDenoising():
     def __init__(self, batch_size, batch_size_un, workers):
         super(DataLoaderDenoising, self).__init__()
         self.trainloader = torch.utils.data.DataLoader(NoisyDataset(dataset_dir_sup_1, dataset_dir_sup_2, dataset_dir_sup_3, 'train', (50,50)), batch_size=batch_size, shuffle=True, num_workers=workers, pin_memory=True)
-        self.trainloader_un = torch.utils.data.DataLoader(NoisyDataset(dataset_dir_sup_1, dataset_dir_sup_2, dataset_dir_sup_3, 'train', (50,50)), batch_size=batch_size, shuffle=True, num_workers=workers, pin_memory=True)
-        #self.trainloader_un = torch.utils.data.DataLoader(NoisyDatasetUnsup(dataset_dir_un, dataset_dir_un_coco), batch_size=batch_size_un, shuffle=True, num_workers=workers) # The batch size for unsupervised data is more than supervised data
-        self.validationloader = torch.utils.data.DataLoader(NoisyDatasetUn(dataset_dir_un_1_gt, dataset_dir_un_1_noisy, mode='test', img_size=(480, 320)), batch_size=1, shuffle=True, num_workers=workers)
+        self.trainloader_un = torch.utils.data.DataLoader(NoisyDatasetUn(dataset_dir_un_coco, 'train', (50,50)), batch_size=batch_size_un, shuffle=True, num_workers=workers, pin_memory=True)
+        self.validationloader_cbsd68 = torch.utils.data.DataLoader(NoisyDatasetVal(dataset_dir_val_1_gt, dataset_dir_val_1_noisy, mode='test', img_size=(480, 320)), batch_size=1, shuffle=True, num_workers=workers)
+        self.validationloader_coco = torch.utils.data.DataLoader(NoisyDatasetUn(dataset_dir_un_coco, 'val', (50,50)), batch_size=batch_size_un, shuffle=True, num_workers=workers, pin_memory=True)
         
     def get_trainloader(self):
         return self.trainloader
@@ -28,4 +29,4 @@ class DataLoaderDenoising():
         return self.trainloader_un
     
     def get_validationloader(self):
-        return self.validationloader
+        return self.validationloader_cbsd68
--- datasets/dataset_denoising.py
+++ datasets/dataset_denoising.py
@@ -5,6 +5,7 @@ import numpy as np
 import torchvision as tv
 import random as rand
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+from sklearn.model_selection import train_test_split
 
 def addNoise(x, sigma=None): 
     """
@@ -21,7 +22,7 @@ def addNoise(x, sigma=None):
     return x + (torch.randn(x.shape) * noiseLevel)
 
 class NoisyDataset(torch.utils.data.Dataset):
-  def __init__(self, in_path_data_1, in_path_data_2, in_path_data_3, mode='train', img_size=(320, 320), sigma=30):
+  def __init__(self, in_path_data_1, in_path_data_2, in_path_data_3, mode='train', img_size=(320, 320), batch_size=128):
     super(NoisyDataset, self).__init__()
 
     self.mode = mode #train or test
@@ -29,11 +30,12 @@ class NoisyDataset(torch.utils.data.Dataset):
     self.in_path_data_2 = in_path_data_2
     self.in_path_data_3 = in_path_data_3
     self.img_size = img_size # (180, 180)
+    self.batch_size = batch_size
 
     self.imgs_data_1 = os.listdir(self.in_path_data_1)
     self.imgs_data_2 = os.listdir(self.in_path_data_2)
     self.imgs_data_3 = os.listdir(self.in_path_data_3)
-    #self.sigma = sigma
+
     self.imgs_path = list()
     for i in self.imgs_data_1:
       _, ext = os.path.splitext(i)
@@ -49,11 +51,9 @@ class NoisyDataset(torch.utils.data.Dataset):
         self.imgs_path.append(os.path.join(self.in_path_data_3, i))
 
   def __len__(self):
-    #return len(self.imgs_path)
-    return 8000*128
+    return 8000*self.batch_size
   
   def __getitem__(self, idx):
-    #img_path = os.path.join(self.img_dir, self.imgs[idx])
     idx = idx%len(self.imgs_path)
     img_path = self.imgs_path[idx]
     clean_img = Image.open(img_path).convert('RGB')
@@ -65,12 +65,10 @@ class NoisyDataset(torch.utils.data.Dataset):
         top = np.random.randint(clean_img.size[1] - self.img_size[1])
     else:
         top = 0
-    # .crop(left, upper, right, lower)
     cropped_clean = clean_img.crop([left, top, left+self.img_size[0], top+self.img_size[1]])
    
     transform = tv.transforms.Compose([
                                     tv.transforms.RandomHorizontalFlip(p=0.5),
-                                    #tv.transforms.ToTensor(),
                                     ])
     ground_truth = transform(cropped_clean)
     ground_truth = np.array(cropped_clean) / 255.
@@ -81,9 +79,9 @@ class NoisyDataset(torch.utils.data.Dataset):
     return noisy, ground_truth
 
 
-class NoisyDatasetUn(torch.utils.data.Dataset):
+class NoisyDatasetVal(torch.utils.data.Dataset):
   def __init__(self, in_path_gt, in_path_noisy, mode='train', img_size=(320, 320), sigma=30):
-    super(NoisyDatasetUn, self).__init__()
+    super(NoisyDatasetVal, self).__init__()
 
     self.mode = mode #train or test
     self.in_path_gt = in_path_gt #
@@ -134,47 +132,43 @@ class NoisyDatasetUn(torch.utils.data.Dataset):
     return ground_truth, ground_truth
 
 
-
-
-
-
-
-
-
-
-
-
-class NoisyDatasetUnsup(torch.utils.data.Dataset):
-  def __init__(self, in_path, in_path_coco, mode='train', img_size=(320, 320), sigma=30):
-    super(NoisyDatasetUnsup, self).__init__()
-
-    self.mode = mode #train or test
+class NoisyDatasetUn(torch.utils.data.Dataset):
+  def __init__(self, in_path, mode='train', img_size=(320, 320), batch_size=1280):
+    super(NoisyDatasetUn, self).__init__()
+  
+    self.batch_size = batch_size
     self.in_path = in_path #
-    self.img_size = img_size # (180, 180)
-
-    self.img_dir = os.path.join(in_path, "Data")
-    self.imgs = os.listdir(self.img_dir)
-    i = 0
-    for n in self.imgs:
-      self.imgs[i] = os.path.join(n, "GT_SRGB_010.PNG")
-      i = i+1
-    #self.sigma = sigma
-
+    self.mode = mode #train or test
+    self.imgs = os.listdir(self.in_path)
     self.imgs_path = list()
-    for i in self.imgs:
-      self.imgs_path.append(os.path.join(self.img_dir, i))
+    self.img_size = img_size
 
-    self.in_path_coco = in_path_coco # ./unlabeled2017
-    self.imgs_path += [self.in_path_coco + "/" + f for f in os.listdir(self.in_path_coco ) if os.path.isfile(os.path.join(self.in_path_coco , f))]
+    for i in self.imgs:
+      _, ext = os.path.splitext(i)
+      if ext in [".jpg", ".jpg", ".bmp", ".JPEG", ".jpeg", ".png"]:
+        self.imgs_path.append(os.path.join(self.in_path, i))
+
+    x_train ,x_test = train_test_split(self.imgs_path,test_size=0.3, random_state=42)
+    print("split")
+    print("train")
+    print(len(x_train))
+    print("test")
+    print(len(x_test))
+    if (self.mode == "val"):
+      self.imgs_path = x_test
+    else:
+      self.imgs_path = x_train
 
   def __len__(self):
-    return len(self.imgs_path)
+    if (self.mode == "val"):
+      return len(self.imgs_path)
+    else:
+      return 8000*self.batch_size
 
   def __getitem__(self, idx):
-    #img_path = os.path.join(self.img_dir, self.imgs[idx])
+    idx = idx%len(self.imgs_path)
     img_path = self.imgs_path[idx]
     clean_img = Image.open(img_path).convert('RGB')
-
     if (clean_img.size[0] > self.img_size[0]):
         left = np.random.randint(clean_img.size[0] - self.img_size[0])
     else:
@@ -183,10 +177,15 @@ class NoisyDatasetUnsup(torch.utils.data.Dataset):
         top = np.random.randint(clean_img.size[1] - self.img_size[1])
     else:
         top = 0
-    # .crop(left, upper, right, lower)
     cropped_clean = clean_img.crop([left, top, left+self.img_size[0], top+self.img_size[1]])
-    transform = tv.transforms.Compose([tv.transforms.ToTensor(),
-                                    tv.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+   
+    transform = tv.transforms.Compose([
+                                    tv.transforms.RandomHorizontalFlip(p=0.5),
+                                    ])
     ground_truth = transform(cropped_clean)
-    noisy = addNoise(ground_truth)
+    ground_truth = np.array(cropped_clean) / 255.
+    ground_truth = tv.transforms.ToTensor()(ground_truth)
+    #print(ground_truth[0])
+    #noisy = addNoise(ground_truth)
+    noisy = ground_truth
     return noisy, ground_truth
